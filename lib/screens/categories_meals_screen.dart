4cef73fe6796f5de5124e1ad8746b5c454266c91
Itamar Nunes <itamarbel@gmail.com>
2020-04-28 23:06:49 -0300

    Changed component MealItem with Stack and another widgtes

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../components/meal_item.dart';
+import '../data/dummy_data.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final category = ModalRoute.of(context).settings.arguments as Category;
-    
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category.title),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Receitas por Categoria'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (_, index) {
+            return MealItem(categoryMeals[index]);
+          },
+        ));
   }
 }
